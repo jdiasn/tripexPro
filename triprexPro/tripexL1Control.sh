b18887931f684e7b47dfc6632d9df3211aa4e677
jdiasn <jdiasn@gmail.com>
2017-05-24 13:33:52 +0200

    Working to pass the arguments by Shell

--- triprexPro/tripexL1Control.sh
+++ triprexPro/tripexL1Control.sh
@@ -1,28 +1,69 @@
-#--File Definition
+#--File Definition------------
 inputPath='/home/jdias/Projects/radarData'
-outputPath='/home/jdias/projects/radarDataResample'
+outputPath='/home/jdias/Projects/radarDataResample'
+#-----------------------------
 
-#--Time Definition
+#--Time Definition------------
 year=2015
 month=11
-day=XX
+day=24
 beguinTime=17
 timeFreq=4s
 timeTolerance=2s
+#-----------------------------
 
 #--Range Definition
 beguinRangeRef=100
 endRangeRef=12000
 rangeFreq=30
 rangeTolerance=17
+#-----------------------------
 
-#--X band Radar setup
+#--X band Radar setup---------
+#
 radar='X'
 rangeOffSet=-17.5
-variables=('Ze','vd')
+variables=('Ze' 'vd')
+for variable in ${variables[@]}
+do
+python tripexPro.py $inputPath $outputPath $year $month $day \
+                          $beguinTime $timeFreq $timeTolerance \
+                          $beguinRangeRef $endRangeRef $rangeFreq \
+                          $rangeTolerance $radar $rangeOffSet $variable
+done
+#-----------------------------
+echo Radar: $(tput setaf 3) $radar Done $(tput sgr 0)
 
-#--w band Radar setup
+
+#--W band Radar setup---------
+#
+#radar='W'
+#rangeOffSet=-2
+#variables=('Ze' 'vm' 'sigma')
+#for variable in ${variables[@]}
+#do
+#echo python triprexPro.py $inputPath $outputPath $year $month $day \
+#                          $beguinTime $timeFreq $timeTolerance \
+#                          $beguinRangeRef $endRangeRef $rangeFreq \
+#                          $rangeTolerance $radar $rangeOffSet $variable
+#done
+#-----------------------------
+#echo Radar: $(tput setaf 3) $radar Done $(tput sgr 0)
 
 
+#--W band Radar setup---------
+#
+#radar='Ka'
+#rangeOffSet=0
+#variables=('Zg' 'VELg' 'RMS' 'LDR')
+#for variable in ${variables[@]}
+#do
+#echo python triprexPro.py $inputPath $outputPath $year $month $day \
+#                          $beguinTime $timeFreq $timeTolerance \
+#                          $beguinRangeRef $endRangeRef $rangeFreq \
+#                          $rangeTolerance $radar $rangeOffSet $variable
+#done
+#-----------------------------
+#echo Radar: $(tput setaf 3) $radar Done $(tput sgr 0)
 
 
