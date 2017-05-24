4744710a2d96121babaeb03d71996add333da770
jdiasn <jdiasn@gmail.com>
2017-05-24 10:37:39 +0200

    Initial L1 control file

---
+++ triprexPro/tripexL1Control.sh
@@ -0,0 +1,28 @@
+#--File Definition
+inputPath='/home/jdias/Projects/radarData'
+outputPath='/home/jdias/projects/radarDataResample'
+
+#--Time Definition
+year=2015
+month=11
+day=XX
+beguinTime=17
+timeFreq=4s
+timeTolerance=2s
+
+#--Range Definition
+beguinRangeRef=100
+endRangeRef=12000
+rangeFreq=30
+rangeTolerance=17
+
+#--X band Radar setup
+radar='X'
+rangeOffSet=-17.5
+variables=('Ze','vd')
+
+#--w band Radar setup
+
+
+
+
